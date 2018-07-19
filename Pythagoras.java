e4e865d3fef04d2195a84e207a24963114a5b284
Shivam Kagalkar <shivam.kagalkar@gmail.com>
2018-07-19 21:09:39 +0530

    Add files via upload
    Pythagoras theorem using JOptionPane (swing)

---
+++ Pythagoras.java
@@ -0,0 +1,39 @@
+import javax.swing.JOptionPane;
+
+public class Pythagoras {
+
+	public static double a;
+	public static double b;
+	public static double c;
+	public static void getData()
+	{
+			a=Double.parseDouble(JOptionPane.showInputDialog("Enter Value For A "));
+			b=Double.parseDouble(JOptionPane.showInputDialog("Enter Value For B"));
+	}
+	public static void calcData()
+	{
+		c=Math.sqrt(Math.abs(a*a)+Math.abs(b*b));
+		displayWhatever(c);
+	}
+	public static void displayWhatever(double ans)
+	{
+		JOptionPane.showMessageDialog(null, "Answer IS "+ans);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int chs=JOptionPane.showConfirmDialog(null, "Welcome to Pythagoras Theoram Calculator");
+		if(chs==1)
+		{
+			JOptionPane.showMessageDialog(null, "You Wish to Not Continue");
+			return;
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null, "Let's Begin !");
+		}
+		Pythagoras.getData();
+		Pythagoras.calcData();
+
+	}
+
+}
